efc5b53149607a672fa9692ff36e5b0ac3eb8da6
KillingJoke42 <anantraina1999@gmail.com>
2021-10-26 17:33:51 +0530

    Adds center window for GUI

--- src/simplegui.cpp
+++ src/simplegui.cpp
@@ -31,6 +31,7 @@ int start_form(int argc, char **argv) {
     
     GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "CHIP8 ROM SELECT");
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     // gtk_window_set_default_size(GTK_WINDOW(window), 640, 320);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
